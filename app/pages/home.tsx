6c3f371f76a7f29348b42aada46ca74c55bf2823
rainistiirik23 <rainis.tiirik9@gmail.com>
2026-01-28 16:09:37 +0200

    Initial commit

---
+++ app/pages/home.tsx
@@ -0,0 +1,7 @@
+
+export function Welcome() {
+  return (
+   <div><h1>Welcome home</h1></div>
+  );
+}
+
---
+++ app/routes/home.tsx
@@ -0,0 +1,5 @@
+import { Welcome } from "../pages/home";
+
+export default function Home() {
+  return <Welcome />;
+}
