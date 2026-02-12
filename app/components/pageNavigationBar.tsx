c654e1f3241cc2be650956677f4e92226ba1882c
rainistiirik23 <rainis.tiirik9@gmail.com>
2026-02-12 14:46:32 +0200

    html layouts with icons and classnames

--- app/components/pageNavigationBar.tsx
+++ app/components/pageNavigationBar.tsx
@@ -1,22 +1,70 @@
+import homeIcon from "~/src/assets/icons/homeIcon.svg";
+import contactIcon from "~/src/assets/icons/contactIcon.svg";
+import projectsIcon from "~/src/assets/icons/projectsIcon.svg";
+import skillsIcon from "~/src/assets/icons/skillsIcon.svg";
+import { ReactSVG } from "react-svg";
+
 export function PageNavigationBar() {
   return (
-    <nav>
-      <div>
-        <ul>
-          <li>
-            <a href="/">Kodu</a>
-          </li>
-          <li>
-            <a href="/Skills">Oskused</a>
-          </li>
-          <li>
-            <a href="/Projects">Projektid</a>
-          </li>
-          <li>
-            <a href="/Contact">Kontakt</a>
-          </li>
-        </ul>
-      </div>
+    <nav className="page-navigation-bar">
+      <ul className="page-navigation-bar-unordered-icons-list">
+        <li className="page-navigation-bar-unordered-icon-list-item">
+          <a href="/">
+            <ReactSVG
+              src={homeIcon}
+              beforeInjection={(svg) => {
+                svg.classList.add("page-navigation-bar-svg-icon");
+              }}
+              className="page-navigation-bar-unordered-icon-list-item-container"
+            ></ReactSVG>
+            <span className="page-navigation-bar-unordered-icon-list-item-descriptor">
+              Home
+            </span>
+          </a>
+        </li>
+        <li className="page-navigation-bar-unordered-icon-list-item">
+          <a href="/skills">
+            <ReactSVG
+              src={skillsIcon}
+              beforeInjection={(svg) => {
+                svg.classList.add("page-navigation-bar-svg-icon");
+              }}
+              className="page-navigation-bar-unordered-icon-list-item-container"
+            ></ReactSVG>
+            <span className="page-navigation-bar-unordered-icon-list-item-descriptor">
+              Skills
+            </span>
+          </a>
+        </li>
+        <li className="page-navigation-bar-unordered-icon-list-item">
+          <a href="/projects">
+            <ReactSVG
+              src={projectsIcon}
+              beforeInjection={(svg) => {
+                svg.classList.add("page-navigation-bar-svg-icon");
+              }}
+              className="page-navigation-bar-unordered-icon-list-item-container"
+            ></ReactSVG>
+            <span className="page-navigation-bar-unordered-icon-list-item-descriptor">
+              Project
+            </span>
+          </a>
+        </li>
+        <li className="page-navigation-bar-unordered-icon-list-item">
+          <a href="/contact">
+            <ReactSVG
+              src={contactIcon}
+              beforeInjection={(svg) => {
+                svg.classList.add("page-navigation-bar-svg-icon");
+              }}
+              className="page-navigation-bar-unordered-icon-list-item-container"
+            ></ReactSVG>
+            <span className="page-navigation-bar-unordered-icon-list-item-descriptor">
+              Contact
+            </span>
+          </a>
+        </li>
+      </ul>
     </nav>
   );
 }
--- app/pages/projects.tsx
+++ app/pages/projects.tsx
@@ -1,36 +1,106 @@
-import { HeaderBar } from "~/components/headerBar";
+import { ReactSVG } from "react-svg";
+import { HeaderNavigation } from "~/components/headerNavigation";
 import { PageNavigationBar } from "~/components/pageNavigationBar";
-
+import globeIconLogo from "~/src/assets/icons/globe.svg";
+import githubLogo from "~/src/assets/icons/portfolio-github-repository-icon.svg";
+import exclamationMarkIcon from "~/src/assets/icons/exclamationMarkIcon.svg";
+import { useState } from "react";
 export function Projects() {
+  const [selectedProject, setSelectedProject] = useState<String | undefined>();
   return (
     <>
-      <head>
-        <HeaderBar />
-      </head>
+      <header>
+        <HeaderNavigation />
+      </header>
       <main>
-        <div>
-          <h1>Projektid</h1>
+        <div className="page-heading-container">
+          <h1 className="page-heading">Projektid</h1>
         </div>
-        <div>
-          <ul>
-            <li>
-              <div>
-                <h2>Title</h2>
-                <p>description</p>
-                <div>
-                  <ul>
-                    <li>html</li>
-                    <li>html</li>
-                    <li>html</li>
-                    <li>html</li>
-                    <li>html</li>
-                    <li>html</li>
-                    <li>html</li>
-                    <li>html</li>
-                  </ul>
-                </div>
-                <div>
-                  <img src={undefined} alt="" />
+        <div className="projects-container">
+          <ul className="unordered-project-list-container">
+            <li className="unordered-project-list-item">
+              <div className="project-container">
+                <h2 className="project-title">Projekti tiitel</h2>
+                <div className="project-details-container">
+                  <div>
+                    <p className="project-description">Projekti kirjeldus</p>
+                    <div className="unordered-used-tool-list-container">
+                      <ul className="unordered-used-tool-list">
+                        <li className="unordered-used-tool-list-item">
+                          <span className="used-tool-name-for-project">
+                            Javascript
+                          </span>
+                        </li>
+                        <li className="unordered-used-tool-list-item">
+                          <span className="used-tool-name-for-project">
+                            Remix
+                          </span>
+                        </li>
+                        <li className="unordered-used-tool-list-item">
+                          <span className="used-tool-name-for-project">
+                            Node js
+                          </span>
+                        </li>
+                        <li className="unordered-used-tool-list-item">
+                          <span className="used-tool-name-for-project">
+                            express
+                          </span>
+                        </li>
+                        <li className="unordered-used-tool-list-item">
+                          <span className="used-tool-name-for-project">
+                            Discord js
+                          </span>
+                        </li>
+                        <li className="unordered-used-tool-list-item">
+                          <span className="used-tool-name-for-project">
+                            html
+                          </span>
+                        </li>
+                        <li className="unordered-used-tool-list-item">
+                          <span className="used-tool-name-for-project">
+                            html
+                          </span>
+                        </li>
+                        <li className="unordered-used-tool-list-item">
+                          <span className="used-tool-name-for-project">
+                            html
+                          </span>
+                        </li>
+                      </ul>
+                    </div>
+                    <div className="project-thumbnail"></div>
+                    <div className="project-unordered-icons-list-container">
+                      <ul className="project-icons-unordered-list">
+                        <li>
+                          <ReactSVG
+                            beforeInjection={(svg) =>
+                              svg.classList.add("project-icon")
+                            }
+                            src={githubLogo}
+                          ></ReactSVG>
+                        </li>
+                        <li>
+                          <ReactSVG
+                            onClick={() => {
+                              setSelectedProject("destiny-vendorbot");
+                            }}
+                            beforeInjection={(svg) =>
+                              svg.classList.add("project-icon")
+                            }
+                            src={exclamationMarkIcon}
+                          ></ReactSVG>
+                        </li>
+                        <li>
+                          <ReactSVG
+                            beforeInjection={(svg) =>
+                              svg.classList.add("project-icon")
+                            }
+                            src={globeIconLogo}
+                          ></ReactSVG>
+                        </li>
+                      </ul>
+                    </div>
+                  </div>
                 </div>
               </div>
             </li>
